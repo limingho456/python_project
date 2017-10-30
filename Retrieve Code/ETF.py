53967c72ac2de5092a63a03c5440d120835260d1
MINGHO LI <mingholi4-c@my.cityu.edu.hk>
2017-10-31 01:07:26 +0800

    Check In

---
+++ Retrieve Code/ETF.py
@@ -0,0 +1,7 @@
+import datetime as dt
+import pandas_datareader.data as pd_reader
+import matplotlib.pyplot as mp_pyplot
+
+#df = df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
+#df.columns = df.ix[0]
+#df.drop(df.index[0], inplace=True)
