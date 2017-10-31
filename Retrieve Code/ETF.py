506808ae7d8a8f2e80767bcda4d92383d4be37aa
LI MINGHO <michael_li@LIs-MacBook-Pro.local>
2017-10-31 19:01:20 +0800

    Test

--- Retrieve Code/ETF.py
+++ Retrieve Code/ETF.py
@@ -1,7 +1,8 @@
 import datetime as dt
+import pandas as pd
 import pandas_datareader.data as pd_reader
 import matplotlib.pyplot as mp_pyplot
 
-#df = df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
-#df.columns = df.ix[0]
-#df.drop(df.index[0], inplace=True)
+df = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]
+df.columns = df.ix[0]
+df.drop(df.index[0], inplace=True)
