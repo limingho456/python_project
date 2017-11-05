8baf632ac7b889c88a465c53c0dcf17d17cded76
MINGHO LI <mingholi4-c@my.cityu.edu.hk>
2017-11-05 21:59:17 +0800

    Update 11/5/2017 -version 2

--- Example/Example1.py
+++ Example/Example1.py
@@ -1,3 +1,8 @@
+"""
+This is example
+"""
+#pylint: disable=invalid-name
+
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -5,13 +10,14 @@ import pandas as pd
 import pandas_datareader.data as web
 
 style.use("ggplot")
-start = dt.datetime(2017,10,1)
-end = dt.datetime(2017,11,1)
+start = dt.datetime(2017, 10, 1)
+end = dt.datetime(2018, 1, 1)
 #Get data from yahoo finance
-#df = web.DataReader("AMZN", "yahoo", start, end)
-#df.to_csv("amzn.csv")
+df = web.DataReader("0700.hk", "yahoo", start, end)
+df.to_csv("0700.csv")
 #Read data from csv file
-df = pd.read_csv("amzn.csv", parse_dates=True, index_col=0)
-print(df.tail(6))
+df = pd.read_csv("0700.csv", parse_dates=True, index_col=0)
+df = df.round(2)
+print(df.tail())
 
 print("Program finish without error.")
