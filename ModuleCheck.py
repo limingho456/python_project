9e8df1944c9ce47f9157d2a37308c834266d9a71
LI MINGHO <mingholi4-c@my.cityu.edu.hk>
2017-11-06 00:21:17 +0800

    11/5/2017 -version 3

--- ModuleCheck.py
+++ ModuleCheck.py
@@ -1,6 +1,7 @@
 #Check python module installed in local machine
 #Necessary module: Numpy, Scipy, Matplotlib, Pandas, Pandas_datareader, scikit_learn
 import pip
+import matplotlib
 
 installed_packages = pip.get_installed_distributions()
 installed_packages_list = sorted(["Module:%s, version:%s" % (i.key, i.version) for i in installed_packages])
