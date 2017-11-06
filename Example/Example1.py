707a640465b2ef70f7260c48f745b712416b8129
MINGHO LI <mingholi4-c@my.cityu.edu.hk>
2017-11-07 02:53:27 +0800

    11/7/2017 -version 3

--- Example/Example1.py
+++ Example/Example1.py
@@ -15,17 +15,20 @@ end = dt.datetime(2018, 1, 1)
 #Get data from yahoo finance
 #df = web.DataReader("0700.hk", "yahoo", start, end)
 #df.to_csv("0700.csv")
+
 #Read data from csv file
 df = pd.read_csv("0700.csv", parse_dates=True, index_col=0)
-#df = df.round(2)
-df['Average'] = (df['High'] + df['Low'] + df['Adj Close'])/3
+df['Avg Price'] = (df['High'] + df['Low'] + df['Adj Close']) / 3
 df['10MA'] = df['Adj Close'].rolling(window=10).mean()
 df['20MA'] = df['Adj Close'].rolling(window=20).mean()
 df['50MA'] = df['Adj Close'].rolling(window=50).mean()
 df['250MA'] = df['Adj Close'].rolling(window=250).mean()
-df['SD'] = df['Average'].rolling(window=20).std()
-df['Lower'] = df['20MA'] - 2 * df['SD']
-df['Upper'] = df['20MA'] + 2 * df['SD']
+df = df.round(2)
+
+df['SD'] = df['Avg Price'].rolling(window=20).std()
+df['Middle'] = df['Avg Price'].rolling(window=20).mean()
+df['Lower'] = df['Middle'] - 2 * df['SD']
+df['Upper'] = df['Middle'] + 2 * df['SD']
 print(df.tail(20))
 
 print("Program finish without error.")
